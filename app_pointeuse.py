75f25d23d410c53b3d988b56a2bf6483ee0c5544
Jerome Bousquie <jerome@bousquie.fr>
2023-05-15 16:28:30 +0200

    ajout du log et de retentative

--- app_pointeuse.py
+++ app_pointeuse.py
@@ -22,8 +22,8 @@ app = Flask(__name__)
 
 BASEURL = 'https://filou.iut-rodez.fr/pointe/'
 
-MAX_ATTEMPTS = 5
-DELAY_INC = 0.2
+MAX_ATTEMPTS = 1
+DELAY_INC = 0.5
 
 @app.route('/generate_url', methods = ['POST'])
 def generate_url():
@@ -57,26 +57,43 @@ def pointe(login, key, encoded):
     action_url =  "https://ohris.ut-capitole.fr/fr/time/punch/add_virtual"
 
     attempt = 0
-    delay = 0
+    delay = 2
     msg = ''
+    log = '\n\n'
     unsuccessful = True
     while attempt < MAX_ATTEMPTS and unsuccessful:
         time.sleep(delay)
 
         sa = CasAuthenticator.auth_webapp.auth_service(service, login, password)
-        action = CasAuthenticator.auth_webapp.exec_auth_action(sa, action_url, headers_punch)
-        json_msg = action.text
-        try:
-            obj_msg = json.loads(json_msg)
-            if obj_msg['result']== "success":
-                unsuccessful = False
-            enc_msg = obj_msg['message']
-            msg = bytes(enc_msg, 'utf-8').decode()
-        except ValueError as e:
-            msg = json_msg     
+        if sa.status_code == 200:
+            time.sleep(2)
+            action = CasAuthenticator.auth_webapp.exec_auth_action(sa, action_url, headers_punch)
+            if action.status_code == 200:
+                json_msg = action.text
+                try:
+                    obj_msg = json.loads(json_msg)
+                    if obj_msg['result']== "success":
+                        unsuccessful = False
+                    enc_msg = obj_msg['message']
+                    msg = bytes(enc_msg, 'utf-8').decode()
+                    m = 'Action réussie\n'
+                    log = log + m
+                except ValueError as e:
+                    m = 'décodage JSON impossible.\n'
+                    log = log + m
+                    msg = json_msg 
+            else: 
+                m = 'app_pointeurse.py - exec_auth_ection échoué  essai : ' + str(attempt + 1) + ' status code : ' + str(action.status_code) + '\n'
+                log = log + m
+                print(m) 
+        else:
+            m = 'app_pointeuse.py - auth_service échoué essai global : ' + str(attempt + 1) + ' après 4 tentatives CAS+Ohris, status code : ' + str(sa.status_code) + '\n'
+            log = log + m
+            print(m)
         attempt = attempt + 1
         delay = delay + DELAY_INC
-
+    
+        msg = msg + log
     return msg 
 
 
